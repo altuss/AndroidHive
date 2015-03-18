d01422f07e15ca1ec21df954989ef49cfaebf6e9
winterssky <ik15209@etf.unsa.ba>
2015-03-18 23:02:35 +0100

    m

--- LRT_Layout/app/src/main/java/com/irvin/lrt_layout/LayoutsActivity.java
+++ LRT_Layout/app/src/main/java/com/irvin/lrt_layout/LayoutsActivity.java
@@ -11,7 +11,7 @@ public class LayoutsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layoutori);
+        setContentView(R.layout.table_layout);
     }
 
     @Override
